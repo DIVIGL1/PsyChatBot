2b3f0cde63cd2a7d3ba63ef9330eb1fb572dc02a
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2023-02-26 06:00:27 +0300

    Added adding one of the tokens (positive, negative) to clarify the content of the proposal for rejection or its absence.

--- text_processing.py
+++ text_processing.py
@@ -1,6 +1,4 @@
-# Необходимо один зар загрузить корпус стоп слов,
-# то есть надо раскомментировать две следующие троки.
-
+# Необходимо один раз загрузить корпус "стоп слов".
 # import nltk
 # nltk.download("stopwords")
 
@@ -9,15 +7,24 @@ from pymystem3 import Mystem
 from string import punctuation
 
 mystem = Mystem()
-russian_stopwords = set(stopwords.words("russian")) ^ {"не", "нет"}
-# russian_stopwords = stopwords.words("russian")
+try:
+    print("Подготавливаем стоп-слова.")
+    russian_stopwords = set(stopwords.words("russian")) ^ {"не", "нет", "хорошо"}
+except:
+    print("Загружаем стоп-слова...")
+    import nltk
+    nltk.download("stopwords")
+    russian_stopwords = set(stopwords.words("russian")) ^ {"не", "нет", "хорошо"}
 
 
 def preprocess_text(text):
     tokens = mystem.lemmatize(text.lower())
-    tokens = [token for token in tokens if token not in russian_stopwords \
-              and token != " " \
-              and token.strip() not in punctuation]
+    tokens = [token for token in tokens if token not in russian_stopwords and token != " " and token.strip() not in punctuation]
+
+    if ("не" in tokens) or ("нет" in tokens):
+        tokens = tokens + ["negative"]
+    else:
+        tokens = tokens + ["positive"]
 
     text = " ".join(tokens)
 
